ea2e988742f5fefd57f56f2f67f6a9075b5b00ba
kereminci <54918783+kereminci@users.noreply.github.com>
2022-05-15 20:16:09 +0300

    hakkımızdaki sayfası text degisti

--- src/pages/Hakkimizda.js
+++ src/pages/Hakkimizda.js
@@ -16,8 +16,19 @@ function Hakkimizda() {
           <h1 className="h1font fw-bold mt-5">HAKKIMIZDA</h1>
           <hr className="border-2 border-top border-primary w-100 "></hr>
             <div className="row">
-              <p>Bilgisayar Mühendisliği Bilimi, bilgisayar donanım ve yazılım sistemleri ile bu sistemlerin uygulamalarından oluşur. Bilgisayar Mühendisliği Programı, öğrencilere bilgisayar yazılımı ve donanımı ile bilgisayar uygulamaları alanlarında dengeli temel bilgileri verecek şekilde tasarlanmıştır. Öğrencilere meslek yaşamlarında, hızla değişen bilgisayar teknolojisine uyum sağlayabilecekleri esnek bir ders programı uygulanmaktadır. Bölümüzdeki eğitim, esas olarak temel matematik dersleri, bilgisayar yazılım ve donanım sistemlerinin analizi ve tasarımı ile yapay zeka, yapay sinir ağları, internet teknolojileri gibi uzmanlık konularında yoğunlaşmıştır. İş dünyasına, Veri Tabanı, Bilişim Sistemleri ve Bilgisayar Uygulamaları ile destek verir. Bilgisayar Mühendisliği Bölümü, Kamu sektöründe, Endüstride, Bilgisayar Sistemleri Tasarlayan, Geliştiren, Satan Kuruluşlarda çalışmak isteyen uzmanlar yetiştirmektedir.</p>
-              
+            <p>
+            Misyon <br/>
+
+İstanbul Üniversitesi-Cerrahpaşa; geçmişten gelen birikimi ile sağlık, mühendislik, doğa ve sosyal bilimler alanlarında tercih edilen, etik değerlere bağlı, kaliteli hizmetler sunan, sosyal ve akademik donanımlı bireyler yetiştiren, bilgi birikiminden istifade edilen, “en iyi olmak için” sloganıyla hareket eden köklü bir araştırma üniversitesidir.
+
+<br/>
+Vizyon
+<br/>
+
+Dünyada ve ülkemizde kabul edilen öncü bir araştırma üniversitesi olmak.
+
+
+              </p>
               <img src="https://i.hizliresim.com/jc1xt7t.jpg" className='w-25 rounded shadow-lg px-0 mx-5'/>
             
               
@@ -29,13 +40,18 @@ function Hakkimizda() {
             <hr className="border-2 border-top border-primary w-100 "></hr>
             <div className='row'>
               
-              <p>Bilgisayar Mühendisliği alanında bilimsel ve analitik düşünceye dayalı lisans eğitimi ve araştırma faaliyetlerinin yürütülmesini sağlayarak evrensel nitelikte rekabet gücüne sahip bilim insanları ve uzmanlar yetiştirmektir.
-              
-              Bilgisayar Mühendisliği alanında verdiği eğitimle, dünyada önde gelen bölümler arasında yer almak ve yetiştirilen öğrenciler ile piyasadaki gerekli bilgi, beceri ve yetkinliklere sahip eleman gereksinimini karşılamada öncü olmak.
-              Bilgisayar Mühendisliği alanında verdiği eğitimle, dünyada önde gelen bölümler arasında yer almak ve yetiştirilen öğrenciler ile piyasadaki gerekli bilgi, beceri ve yetkinliklere sahip eleman gereksinimini karşılamada öncü olmak.
-              Bilgisayar Mühendisliği alanında verdiği eğitimle, dünyada önde gelen bölümler arasında yer almak ve yetiştirilen öğrenciler ile piyasadaki gerekli bilgi, beceri ve yetkinliklere sahip eleman gereksinimini karşılamada öncü olmak.
-              Bilgisayar Mühendisliği alanında verdiği eğitimle, dünyada önde gelen bölümler arasında yer almak ve yetiştirilen öğrenciler ile piyasadaki gerekli bilgi, beceri ve yetkinliklere sahip eleman gereksinimini karşılamada öncü olmak.
+              <p>
+              İstanbul Üniversitesi-Cerrahpaşa, İstanbul'da yer alan bir devlet üniversitesidir.
+
+              5 Mayıs 2018 tarihli, 30425 sayılı Resmî Gazete'de yayımlanarak yürürlüğe giren 7141 sayılı kanun ile İstanbul Üniversitesi'nin bazı fakülte, enstitü, yüksekokul ve meslek yüksekokulları bu üniversiteden ayrılarak "İstanbul Üniversitesi-Cerrahpaşa" adıyla kurulan yeni üniversitenin çatısı altında faaliyet göstermeye başlamıştır. İsmini İstanbul'daki Cerrahpaşa semtine de adını veren Cerrah Mehmed Paşa'dan almaktadır. Türkçe ve İngilizce olmak üzere iki dilde eğitim verir.
+
+              Üniversitenin sekiz yerleşkesi vardır; rektörlüğü Avcılar Yerleşkesindedir.
+
+              Bünyesinde 9 fakülte, 6 enstitü, 1 yüksekokul, 5 meslek yüksekokulu ve 10 araştırma merkezi bulunur. Bu kuruma bağlı olan Cerrahpaşa Tıp Fakültesi, en çok spesifik bilim dalı bulunduran tıp fakültesidir[kaynak belirtilmeli], birçok tıbbi teknolojik yeniliği ülkeye getirmesiyle bilinir. Üniversite'nin kuruluş tarihi logosunda "1453" olarak duyurulmaktadır. Tıp Fakültesi'nin kuruluş tarihi ise, 2019 yılında üniversite senatosu tarafından alınan kararla 1827 olarak kabul edilmiştir.
+
+              26 Eylül 2017 tarihinde Cumhurbaşkanlığı Külliyesi'nde düzenlenen 2017-2018 Akademik Yılı Açılış Töreni'nde İstanbul Üniversitesi-Cerrahpaşa'nın, YÖK tarafından yükseköğretime çeşitliliği getiren “Misyon Farklılaşması ve İhtisaslaşma Projesi” kapsamında belirlenen “Araştırma ve Aday Araştırma Üniversiteleri”nden biri olduğu Cumhurbaşkanı Recep Tayyip Erdoğan tarafından ilan edilmiştir.
               </p>
+              <br/>
               
              
             </div>
