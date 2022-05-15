8e43e7c161769c1cb5dffcaf1f8dd1d1f7289b99
Eren <erennelma@gmail.com>
2022-05-15 19:48:08 +0300

    duyurular sayfası düzenlendi

--- src/pages/Duyurular.js
+++ src/pages/Duyurular.js
@@ -3,12 +3,14 @@
 function Duyurular () {
 
     return (
-        <div class="container mt-3">
-  <h2>Duyurular</h2>
+        <div>
+
+          <br /><br /><br />
+  <div className="container my-5">
+  <h1>Duyurular</h1>
   <table class="table text-start">
     <thead>
       <tr>
-       
       </tr>
     </thead>
     <tbody>
@@ -50,6 +52,7 @@ function Duyurular () {
       </tr>
     </tbody>
   </table>
+  </div>
 </div>
     )
 
