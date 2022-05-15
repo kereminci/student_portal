8aa118e868bfe30295f28ca6890c3086ed007fc9
Eren <erennelma@gmail.com>
2022-05-15 19:41:22 +0300

    hakkımızda sayfası eklendi

--- src/pages/Hakkimizda.js
+++ src/pages/Hakkimizda.js
@@ -1,9 +1,52 @@
 import React from 'react'
 
-const Hakkimizda = () => {
+
+function Hakkimizda() {
   return (
-    <div>Hakkimizda</div>
-  )
+    <div>
+     
+  
+  <br/><br/> <br/> <br/>
+      
+     
+      
+        <div className="album">
+          <div className="container">
+          
+          <h1 className="h1font fw-bold mt-5">HAKKIMIZDA</h1>
+          <hr className="border-2 border-top border-primary w-100 "></hr>
+            <div className="row">
+              <p>Bilgisayar Mühendisliği Bilimi, bilgisayar donanım ve yazılım sistemleri ile bu sistemlerin uygulamalarından oluşur. Bilgisayar Mühendisliği Programı, öğrencilere bilgisayar yazılımı ve donanımı ile bilgisayar uygulamaları alanlarında dengeli temel bilgileri verecek şekilde tasarlanmıştır. Öğrencilere meslek yaşamlarında, hızla değişen bilgisayar teknolojisine uyum sağlayabilecekleri esnek bir ders programı uygulanmaktadır. Bölümüzdeki eğitim, esas olarak temel matematik dersleri, bilgisayar yazılım ve donanım sistemlerinin analizi ve tasarımı ile yapay zeka, yapay sinir ağları, internet teknolojileri gibi uzmanlık konularında yoğunlaşmıştır. İş dünyasına, Veri Tabanı, Bilişim Sistemleri ve Bilgisayar Uygulamaları ile destek verir. Bilgisayar Mühendisliği Bölümü, Kamu sektöründe, Endüstride, Bilgisayar Sistemleri Tasarlayan, Geliştiren, Satan Kuruluşlarda çalışmak isteyen uzmanlar yetiştirmektedir.</p>
+              
+              <img src="https://i.hizliresim.com/jc1xt7t.jpg" className='w-25 rounded shadow-lg px-0 mx-5'/>
+            
+              
+              <img src="https://i.hizliresim.com/k79pdzm.JPG" className='w-25 rounded shadow-lg px-0 mx-5'/>       
+              
+              <img src="https://i.hizliresim.com/2o1g4bg.jpg" className='w-25 rounded shadow-lg px-0 mx-5'/> 
+            </div>
+            <h1 className="h1font fw-bold mt-5">MİSYONUMUZ VE VİZYONUMUZ</h1>
+            <hr className="border-2 border-top border-primary w-100 "></hr>
+            <div className='row'>
+              
+              <p>Bilgisayar Mühendisliği alanında bilimsel ve analitik düşünceye dayalı lisans eğitimi ve araştırma faaliyetlerinin yürütülmesini sağlayarak evrensel nitelikte rekabet gücüne sahip bilim insanları ve uzmanlar yetiştirmektir.
+              
+              Bilgisayar Mühendisliği alanında verdiği eğitimle, dünyada önde gelen bölümler arasında yer almak ve yetiştirilen öğrenciler ile piyasadaki gerekli bilgi, beceri ve yetkinliklere sahip eleman gereksinimini karşılamada öncü olmak.
+              Bilgisayar Mühendisliği alanında verdiği eğitimle, dünyada önde gelen bölümler arasında yer almak ve yetiştirilen öğrenciler ile piyasadaki gerekli bilgi, beceri ve yetkinliklere sahip eleman gereksinimini karşılamada öncü olmak.
+              Bilgisayar Mühendisliği alanında verdiği eğitimle, dünyada önde gelen bölümler arasında yer almak ve yetiştirilen öğrenciler ile piyasadaki gerekli bilgi, beceri ve yetkinliklere sahip eleman gereksinimini karşılamada öncü olmak.
+              Bilgisayar Mühendisliği alanında verdiği eğitimle, dünyada önde gelen bölümler arasında yer almak ve yetiştirilen öğrenciler ile piyasadaki gerekli bilgi, beceri ve yetkinliklere sahip eleman gereksinimini karşılamada öncü olmak.
+              </p>
+              
+             
+            </div>
+            <img src="https://i.hizliresim.com/pvh4dst.png" className='w-25 rounded shadow-lg px-0 mx-5'/> 
+          </div>
+        </div>
+      <br /> <br />
+      
+
+    </div>
+  );
 }
 
-export default Hakkimizda
+export default Hakkimizda;
